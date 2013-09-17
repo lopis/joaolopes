0c8d39572c4da130ecc193c0b822e20beb98552d
João Lopes <joaopclopes@gmail.com>
2013-09-17 16:02:56 +0100

    Lots of changes. New menu in small screens.

--- cv.php
+++ cv.php
@@ -1,1 +1,78 @@
-cvphp
+
+<?php 
+	$at = "about";
+	require_once("header.php");
+?>
+
+<div class="content resume">
+	<h1><span class="en">Resume</span><span class="pt">Currículo</span></h1>
+
+	<h2><span class="en">Personal Information</span><span class="pt">Informação pessoal</span></h2>
+	<h3>
+		<span class="en">Name</span>
+		<span class="pt">Nome</span>
+	</h3>
+	<p>
+		João Pedro Camacho Lopes
+	</p>
+	<h3>
+		<span class="en">Address</span>
+		<span class="pt">Morada</span>
+	</h3>
+	<p>
+		Est. Comandante Camacho Freitas 128
+		<br>Funchal, Portugal
+	</p>
+	<h3>
+		E-mail
+	</h3>
+	<p>
+		<a href="mailto:ei10009@fe.up.pt">ei10009@fe.up.pt</a>
+	</p>
+	<h3>
+		<span class="en">Date of Birth</span>
+		<span class="pt">Data de Nascimento</span>
+	</h3>
+	<p>
+		<span class="en">September 13th 1990</span>
+		<span class="pt">13 de Semtembro de 1990</span>
+	</p>
+	<h3>
+		<span class="en">Nationality</span>
+		<span class="pt">Nacionalidade</span>
+	</h3>
+	<p>
+		<span class="en">Portuguese</span>
+		<span class="pt">Portuguesa</span>
+	</p>
+
+	<h2><span class="en">Education</span><span class="pt">Educação</span></h2>
+	<h3>
+		2008-2010
+	</h3>
+	<p>
+		<span class="en">1st Cycle - Informatics Engineering</span>
+		<span class="pt">Licenciatura em Engenharia Informática</span>
+		<br>Universidade da Madeira
+	</p>
+	<h3>
+		2010-2013
+	</h3>
+	<p>
+		<span class="en">Master in Informatics and Computing Engineering</span>
+		<span class="pt">Mestrado em Engenharia Informática e Computação</span>
+		<br>
+		<span class="en">Faculty of Engineering of</span>
+		<span class="pt">Faculdade de Engenharia da</span>
+		Universidade do Porto
+	</p>
+
+	
+
+</div>
+
+
+
+
+<?php require_once("footer.html") ?>
+
--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="logo span9">
-				<img src="img/joaolopes.png" alt="">
+				<a href="index.php"><img src="img/joaolopes.png" alt=""></a>
 			</div>
 			<div class="menu span3">
 				<ul class="nav">
@@ -22,28 +22,28 @@
 						<a href="about.php" data-toggle="tab" class="nav-lang">
 							<div class="icon"><i class="icon-user icon-2x"></i></div>
 							<div class="en">About</div><div class="pt">Sobre</div>
-							<div class="clearfix"></div>
+							
 						</a>
 					</li>
 					<li <?php if($at=='portfolio') echo 'class="active"'; ?>>
 						<a href="portfolio.php" data-toggle="tab" class="nav-lang">
 							<div class="icon"><i class="icon-briefcase icon-2x"></i></div>
 							<div class="en">Portfolio</div><div class="pt">Portólio</div>
-							<div class="clearfix"></div>
+							
 						</a>
 					</li>
 					<li <?php if($at=='cuv') echo 'class="active"'; ?>>
 						<a href="cv.php" data-toggle="tab" class="nav-lang">
 							<div class="icon"><i class="icon-tasks icon-2x"></i></div>
-							<div class="en">Curriculum</div><div class="pt">Currículo</div>
-							<div class="clearfix"></div>
+							<div class="en">Resume</div><div class="pt">Currículo</div>
+							
 						</a>
 					</li>
 					<li <?php if($at=='contact') echo 'class="active"'; ?>>
 						<a href="about.php#contact" data-toggle="tab" class="nav-lang">
 							<div class="icon"><i class="icon-envelope icon-2x"></i></div>
 							<div class="en">Contacts</div><div class="pt">Contacto</div>
-							<div class="clearfix"></div>
+							
 						</a>
 					</li>
 					<div class="flags">
@@ -52,6 +52,5 @@
 					</div>
 				</ul>
 			</div>
-			<div class="clearfix"></div>
 		</div>
 		<div class="content">
